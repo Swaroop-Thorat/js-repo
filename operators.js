02aabf80cd049141b3e1d74bb0023656f54df97b
Swaroop Thorat <sthorat6663@gmail.com>
2026-07-13 21:10:01 +0530

    operators,js

---
+++ operators.js
@@ -0,0 +1,39 @@
+let a=45
+let b=4
+
+//arithmetic op
+console.log("Add:"+(a+b))
+console.log("Sub:"+(a-b))
+console.log("Mult:"+(a*b))
+console.log("Div:"+(a/b))
+console.log("pow:"+(a**b))
+console.log("mod:"+(a%b))
+console.log("pre inc:"+(b++))
+console.log("post inc:"+(++a))
+console.log("pre dec:"+(b--))
+console.log("post dec:"+(--a))
+
+//assignment op
+let c=a+""
+
+
+// comparison op
+console.log(a>=b)
+console.log(a>b)
+console.log(a<b)
+console.log(a<=b)
+console.log(a===c)
+console.log(a==c)
+console.log(a!=c)
+console.log(a!==c)
+
+
+//logical op
+
+const log1=true
+const log2=false
+
+console.log()
+console.log(log1 && log2)
+console.log(log1 || log2)
+console.log(!log1)
