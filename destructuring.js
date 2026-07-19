38a89303a715800d1aba2b1fa092d2c4b01c25c5
Swaroop Thorat <sthorat6663@gmail.com>
2026-07-19 20:10:31 +0530

    destructing in js

---
+++ destructuring.js
@@ -0,0 +1,14 @@
+let user={name:"Ranveer",addr:"Baroda"}
+let {name,address}=user
+console.log(name)
+
+
+let colours={
+    "red":3,
+    "blue":1,
+    "green":9
+}
+
+let {"red":c2,"blue":c1,"green":c3}=colours
+
+console.log(c1+" "+c2+" "+c3)
